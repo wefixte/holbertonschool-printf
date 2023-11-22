14c0586a1518439a8181474d704b6a53ccca7fe3
wefixte <cassy.pernelle@gmail.com>
2023-11-22 11:42:53 +0100

    Modification documentation _printf function

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _printf - function that produces output according to a format
+ * @format: string
+ * Return: number of characters printed
+*/
+
+int _printf(const char *format, ...)
+{
+	
+}
