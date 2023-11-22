0f93fa6dafb1f17c6dc2b16658696035f0d25a7b
wefixte <cassy.pernelle@gmail.com>
2023-11-22 12:15:49 +0100

    Modif documentation and prototype

---
+++ _getprintfunctions.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _getprintfunctions - search for a specifier and the function linked to it
+ * 
+ * Return: the function linked to the specifier or NULL
+*/
+
+int (*getprintfunctions)(va_list)
+{
+	/*TODO : tableau structure spectifiers*/
+
+	while
+	{
+		/*TODO : boucler dans la structure format pour trouver un match et return la fonction*/
+		if()
+		{
+
+		}
+		else
+			return (NULL);
+	}
+}
