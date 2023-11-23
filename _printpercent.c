17cd8ac86079040f7cd4a2dad9a3e3b0bc34f683
CarmenLeila <carmenmugisha18@gmail.com>
2023-11-23 11:08:16 +0100

    create function that prints percent

---
+++ _printpercent.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _printpercent - function that prints percent
+ * @ListArguments: list of arguments
+ * Return: the length
+*/
+
+int _printpercent(va_list ListArguments)
+{
+    (void) ListArguments;
+        _putchar('%');
+    return(1);
+}
