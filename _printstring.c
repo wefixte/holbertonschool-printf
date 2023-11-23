36666629bd5780a7d4a2b9cdd0a57a5c55754522
wefixte <cassy.pernelle@gmail.com>
2023-11-23 21:35:40 +0100

    betty fix

--- _printstring.c
+++ _printstring.c
@@ -3,23 +3,25 @@
 /**
  * _printstring - function that prints string
  * @ListArguments: list of arguments
- * Return: the length
+ * Return: the length of the string
 */
+
 int _printstring(va_list ListArguments)
 {
-    int index, count = 0;
-    char *str;
-    
-    index = 0;
-    str = va_arg(ListArguments, char*);
-    if(str == NULL)
-        str = "null";
-    while (str[index]!= '\0')
-    {
-            _putchar(str[index]);
-            index++;
-            count++;
-    }
-        return(count);
-        
+	int index = 0;
+	int count = 0;
+	char *str;
+
+	str = va_arg(ListArguments, char*);
+
+	if (str == NULL)
+		str = "null";
+
+	while (str[index] != '\0')
+	{
+		_putchar(str[index]);
+		index++;
+		count++;
+	}
+	return (count);
 }
